de5ac2dbc179006af2c9129d9f884f03943acc60
Simmarith <menzeladrian@googlemail.com>
2025-10-10 19:49:45 +0200

    merge sso auth users

--- convex/auth.ts
+++ convex/auth.ts
@@ -5,6 +5,31 @@ import Google from "@auth/core/providers/google";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
   providers: [Password, Google],
+  callbacks: {
+    async createOrUpdateUser(ctx, args) {
+      if (args.existingUserId) {
+        return args.existingUserId;
+      }
+      
+      if (args.profile?.email) {
+        const existingUser = await ctx.db
+          .query("users")
+          .filter((q) => q.eq(q.field("email"), args.profile.email))
+          .first();
+        
+        if (existingUser) {
+          return existingUser._id;
+        }
+      }
+      
+      const userId = await ctx.db.insert("users", {
+        email: args.profile?.email,
+        name: args.profile?.name,
+        image: args.profile?.image,
+      });
+      return userId;
+    },
+  },
 });
 
 export const loggedInUser = query({
